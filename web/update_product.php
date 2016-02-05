e70d52826ea545eb326884f7956e21c89e67df84
jerome ullmann <jerome.ullmann@gmail.com>
2016-02-05 14:16:26 +0100

    first commit

---
+++ web/update_product.php
@@ -0,0 +1,47 @@
+<?php
+ 
+/*
+ * Following code will update a product information
+ * A product is identified by product id (pid)
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// check for required fields
+if (isset($_GET['pid']) && isset($_GET['name']) && isset($_GET['price']) && isset($_GET['description'])) {
+ 
+    $pid = $_GET['pid'];
+    $name = $_GET['name'];
+    $price = $_GET['price'];
+    $description = $_GET['description'];
+ 
+    // include db connect class
+    require_once __DIR__ . '/db_connect.php';
+ 
+    // connecting to db
+    $db = new DB_CONNECT();
+ 
+    // mysql update row with matched pid
+    $result = mysql_query("UPDATE products SET name = '$name', price = '$price', description = '$description' WHERE pid = $pid");
+ 
+    // check if row inserted or not
+    if ($result) {
+        // successfully updated
+        $response["success"] = 1;
+        $response["message"] = "Product successfully updated.";
+ 
+        // echoing JSON response
+        echo json_encode($response);
+    } else {
+ 
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
