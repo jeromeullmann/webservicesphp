e54445b36301a66f8e861734b4ea9812ee10926c
Jerome Ullmann <jerome.ullmann@gmail.com>
2016-02-10 12:32:04 +0100

    variabilisation en fonction du contexte d'execution

--- web/test_products.php
+++ web/test_products.php
@@ -1,34 +1,42 @@
-
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL")); 
-        
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-        
-echo "db_connect : récupération CLEARDB_DATABASE_URL";
+/* DATABASE_URL => mysql://[username]:[password]@[host]/[database name]?reconnect=true */
+/* Local : mysql://root@localhost/products */
+
+$notfind = True;
+
+$env = getenv('CLEARDB_DATABASE_URL');
+if ($env == "") {
+    echo "execution en local";
+    
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "products";
+    
+} else {
+    
+    $url = parse_url($env);
+    
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    
+}
 
 $mysqli = new mysqli($server, $username, $password, $db);
-
-/* Vérification de la connexion */
+    
 if (mysqli_connect_errno()) {
-    printf("Échec de la connexion : %s\n", mysqli_connect_error());
-    exit();
+      printf("Échec de la connexion : %s\n", mysqli_connect_error());
+      exit();
 }
 
 if ($result = $mysqli->query("SELECT * FROM products")) {
-
-    /* Détermine le nombre de lignes du jeu de résultats */
-    $row_cnt = $result->num_rows;
-
-    printf("Le jeu de résultats a %d lignes.\n", $row_cnt);
-
-    /* Ferme le jeu de résultats */
-    $result->close();
+      $row_cnt = $result->num_rows;
+      printf("Le jeu de résultats a %d lignes.\n", $row_cnt);
+      $result->close();
 }
-
-/* Ferme la connexion */
 $mysqli->close();
 ?>
+
